4887932797b658e8bf426f029ea20874b2e29514
tjay1760 <mcjthiongo@gmail.com>
2023-10-27 14:53:01 +0300

    Implimented API route for reservation index

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ReservationsController < ApplicationController
+    def index
+        @reservations = Reservation.all
+        render json: @reservations
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :houses, only: [:index, :new, :create, :show, :destroy]
+      resources :reservations, only: [:index, :new, :create, :show, :destroy]
     end
   end
 end
