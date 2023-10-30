092ea5b83212f7206954fa920c8f7c9754e84e4c
tonnytech <tonnytei4@gmail.com>
2023-10-30 18:21:49 +0300

    Remove linter errors

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,19 +1,20 @@
 class Api::V1::RegistrationsController < ApplicationController
-    def create
-        user = User.new(
-            name: params["user"] ["name"],
-            email: params["user"] ["email"],
-            password: params["user"] ["password"],
-            password_confirmation: params["user"]["password_confirmation"])
+  def create
+    user = User.new(
+      name: params['user'] ['name'],
+      email: params['user'] ['email'],
+      password: params['user'] ['password'],
+      password_confirmation: params['user']['password_confirmation']
+    )
 
-        if user.save
-            session[:user_id] = user.id
-            render json: {
-                status: :created,
-                user: user
-            }
-        else
-            render json: { errors: user.errors }, status: :unprocessable_entity
-        end
+    if user.save
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        user:
+      }
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
     end
+  end
 end
--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,16 +1,15 @@
 module CurrentUserConcern
-    extend ActiveSupport::Concern
-  
-    included do
-      before_action :set_current_user
-    end
-  
-    private
-  
-    def set_current_user
-      if session[:user_id]
-        @current_user = User.find(session[:user_id])
-      end
-    end
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_current_user
   end
-  
+
+  private
+
+  def set_current_user
+    return unless session[:user_id]
+
+    @current_user = User.find(session[:user_id])
+  end
+end
