2f197089c7aff3e026ddaa92886f270df0247e0e
tonnytech <tonnytei4@gmail.com>
2023-11-08 07:37:39 +0300

    Add user variable to user_controller.rb file

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,21 @@
 class Api::V1::UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
     def create
-        @user = User.create(
+        user = User.create(
           name: params['name'],
           email: params['email'],
           password: params['password'],
           password_confirmation: params['password_confirmation']
         )
       
-        if @user.valid?
+        if user.valid?
             token = encode_token(user_id: user.id)
-            render json: { user: UserSerializer.new(@user), jwt: token }, status: :created
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
         else
-          render json: { error: 'Failed to create user', errors: @user.errors.full_messages }, status: :unprocessable_entity
+          render json: { error: 'Failed to create user', errors: user.errors.full_messages }, status: :unprocessable_entity
         end
+
+        @user =  user
     end
     
     def profile
