a27bf9f3d64cfb62d449cd0cc2632a9620aefd63
Derrick1451 <haylederrick14@gmail.com>
2023-11-09 12:45:53 +0300

    Modify house test model

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -20,10 +20,10 @@ RSpec.describe House, type: :model do
     @house1.house_image = nil
     expect(@house1).to_not be_valid
   end
-  # it 'Icon should be a string' do
-  #   @house1.house_image = 'https://images.unsplash.com/photo-1682686580186-b55d2a91053c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1975&q=80'
-  #   expect(@house1).to be_valid
-  # end
+  it 'Icon should be a string' do
+    @house1.house_image = 'https://images.unsplash.com/photo-1682686580186-b55d2a91053c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1975&q=80'
+    expect(@house1).to be_valid
+  end
   # it 'Should have a name as a string' do
   #   @house1.house_name = 'Milk juice'
   #   expect(@house1).to be_valid
